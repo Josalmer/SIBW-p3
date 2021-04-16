9a35db730677a644162708d884d751487912ddda
Jose Saldaña <jose.saldana@nazaries.com>
2021-04-16 17:34:31 +0200

    Get event images url from database

--- www/models/events.repository.php
+++ www/models/events.repository.php
@@ -2,22 +2,17 @@
     class eventsRepository {
         public function getEvent($evID) {
             if (is_numeric($evID)) {
-                $queryResult = db::getDBSingleton()->query("SELECT id, title, author, body, updated_at FROM events WHERE id = ?", [$evID]);
+                $queryResult = db::getDBSingleton()->query("SELECT id, title, author, body, image_url, updated_at FROM events WHERE id = ?", [$evID]);
 
                 if($queryResult->num_rows > 0) {
-                    $row = $queryResult->fetch_assoc();
-                    $event['id']=$row['id'];
-                    $event['title']=$row['title'];
-                    $event['author']=$row['author'];
-                    $event['body']=$row['body'];
-                    $event['updated_at']=$row['updated_at'];
+                    $event = mysqli_fetch_assoc($queryResult);
                     return $event;
                 }
             }
         }
 
         public function getAllEvents() {
-            $queryResult = db::getDBSingleton()->query("SELECT id, title FROM events", []);
+            $queryResult = db::getDBSingleton()->query("SELECT id, title, image_url FROM events", []);
 
             if($queryResult->num_rows > 0) {
                 $events = $queryResult->fetch_all(MYSQLI_ASSOC);

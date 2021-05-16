d6c82359626356407ef4949d791ea04399672ae7
Jose Saldaña <jose.saldana@nazaries.com>
2021-05-16 16:18:27 +0200

    Removed image url from event

--- www/assets/scripts/gestion.js
+++ www/assets/scripts/gestion.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", () => setGestionListeners());
 
 function setGestionListeners() {
     setEditEventButton();
+
     var eventBody = document.getElementById("event-form-body");
-    if (eventBody) {
-        eventBody.value = eventBody.getAttribute('body');
-    }
+    if (eventBody) { eventBody.value = eventBody.getAttribute('body'); }
+
+    var newEventButton = document.getElementById("new-event-button");
+    if (newEventButton) { newEventButton.addEventListener("click", () => editEvent(null)); }
 }
 
 function setEditEventButton() {
--- www/models/events.repository.php
+++ www/models/events.repository.php
@@ -2,7 +2,7 @@
     class eventsRepository {
         public function getEvent($evID) {
             if (is_numeric($evID)) {
-                $queryResult = db::getDBSingleton()->query("SELECT id, title, author, body, image_url, created_at, updated_at FROM events WHERE id = ?", [$evID]);
+                $queryResult = db::getDBSingleton()->query("SELECT id, title, author, body, created_at, updated_at FROM events WHERE id = ?", [$evID]);
 
                 if($queryResult->num_rows > 0) {
                     $event = mysqli_fetch_assoc($queryResult);
@@ -12,6 +12,7 @@
 
                     $gallery = db::getDBSingleton()->query("SELECT id, image_url FROM gallery_images WHERE event_id = ?", [$evID]);
                     $event['gallery'] = $gallery->fetch_all(MYSQLI_ASSOC);
+                    $event['image_url'] = $event['gallery'][0]['image_url'];
 
                     $tags = db::getDBSingleton()->query("SELECT id, tag FROM tags WHERE event_id = ?", [$evID]);
                     $event['tags'] = $tags->fetch_all(MYSQLI_ASSOC);
@@ -22,10 +23,17 @@
         }
 
         public function getAllEvents() {
-            $queryResult = db::getDBSingleton()->query("SELECT id, title, author, body, image_url FROM events", []);
+            $queryResult = db::getDBSingleton()->query("SELECT id, title, author, body FROM events", []);
 
             if($queryResult->num_rows > 0) {
                 $events = $queryResult->fetch_all(MYSQLI_ASSOC);
+                foreach ($events as &$event) {
+                    $id = $event['id'];
+                    $gallery = db::getDBSingleton()->query("SELECT id, image_url FROM gallery_images WHERE event_id = ? LIMIT 1", [$id]);
+                    $image = $gallery->fetch_all(MYSQLI_ASSOC);
+                    $event['image_url'] = $image[0]['image_url'];
+                }
+                
                 return $events;
             }
         }
